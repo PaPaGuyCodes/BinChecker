8e02df3949eca1c061f6a6995f66b317f586dba4
PaPaGuy Codes  / <175421249+PaPaGuyCodes@users.noreply.github.com>
2024-08-12 09:40:16 +0100

    Update BinData.kt

--- app/src/main/java/com/papaguycodes/binchecker/models/BinData.kt
+++ app/src/main/java/com/papaguycodes/binchecker/models/BinData.kt
@@ -1,8 +1,20 @@
 package com.papaguycodes.binchecker.models
 
 data class BinData(
-    val bin: String,
-    val bankName: String,
-    val cardType: String,
-    val country: String
+    val scheme: String?,
+    val type: String?,
+    val brand: String?,
+    val country: Country?,
+    val bank: Bank?
+)
+
+data class Country(
+    val name: String?,
+    val emoji: String?
+)
+
+data class Bank(
+    val name: String?,
+    val url: String?,
+    val phone: String?
 )
